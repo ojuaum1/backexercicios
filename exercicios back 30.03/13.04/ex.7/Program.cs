408c23819aab16bbce09021f80131ecf732e6211
joao.oliveiira <joao.oliveiira.pda@gmail.com>
2023-04-17 08:11:45 -0300

    14/04

---
+++ exercicios back 30.03/13.04/ex.7/Program.cs
@@ -0,0 +1,20 @@
+﻿// 7 - Faça um algoritmo para ler 15 números e armazenar em um vetor. Após a leitura total dos
+// 15 números, o algoritmo deve escrever esses 15 números lidos na ordem inversa da qual foi
+// declarado.
+
+float[] numeros = new float[15];
+//recebe e armazena os nomes
+for (var i = 0; i < 15; i++)
+{
+    Console.WriteLine($"digite os numeros: ");
+    numeros[i] = float.Parse (Console.ReadLine());
+
+}
+//mostra ordem reversa
+Console.WriteLine($"ordem reversa");
+Array.Reverse(numeros);
+foreach (float item in numeros)
+{
+    Console.WriteLine($"{item}");
+    
+}
